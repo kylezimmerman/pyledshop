e7ab5db220e1a28f3850b7d96e7c9e5ee2cf4d64
Kyle Zimmerman <kylejustlovesspam@gmail.com>
2019-07-21 14:46:16 -0400

    Refactor for pip module

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'pyledshop',
+    packages = ['pyledshop'],
+    version = '0.0.1',
+    description = 'A Python library to communicate with LED Shop compatible led strip controllers',
+    author = 'Kyle Zimmerman',
+    url = 'https://github.com/kylezimmerman/pyledshop',
+    license = 'MIT',
+    keywords = [
+        'led shop',
+        'led strip',
+        'light',
+        ]
+)
